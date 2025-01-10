289e83a01aaca89015e0c3712e349bcf3338bd91
Manasi Mithe <manasimithe@gmail.com>
2025-01-10 12:47:10 +0530

    commit

--- basic.py
+++ basic.py
@@ -1,5 +1,46 @@
+# TYPE CASTING
 a = 4
 b=4.5
 c = a+b
 print(c)
 print("hello")
+
+# ARITHMATIC OPERATOR
+a = 2
+b = 4
+print("value is",a,"values is b","addition is =",a+b)
+
+# COMPARISON OPERATOR
+a =4
+b=6
+print(a>b)
+print(b<a)
+
+#IDENTIFY OPERATOR
+a = [1, 2, 3, 4, 5]
+b = [1, 2, 3, 4, 5]
+c = a
+
+print(a is c)
+print(b is c)
+
+print(a is not c)
+print(b is not c)
+
+
+# USER INPUT
+# Name = input("my name is:")
+# address = input("address is:")
+# print(Name)
+# print(address)
+
+
+# INTEGER NUBERS
+a = 5
+b = 67
+c = a+b
+print("a",a,type(a))
+print("c",c,type(c))
+
+
+
